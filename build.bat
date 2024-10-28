f2ffc180e137bbc3bdcdde448b1f1d15f2ca3821
Michael Delfino <m_delfino@outlook.com>
2024-10-27 22:17:13 -0400

    added more build file options, fixed SDL2.dll bug

--- build.bat
+++ build.bat
@@ -2,7 +2,10 @@
 
 mkdir .\build
 pushd .\build
-cl /EHsc ..\CPP_Engine\main.cpp ^
+
+cl /EHsc /JMC /GS /W3 /ZI /Gd /sdl /MDd /FC /Od /fp:precise /WX- /Zc:wchar_t ^
+    /Zc:forScope /RTC1 ^
+            ..\CPP_Engine\main.cpp ^
             ^
             ..\CPP_Engine\HUD.cpp ^
             ..\CPP_Engine\Scene.cpp ^
@@ -24,11 +27,23 @@ cl /EHsc ..\CPP_Engine\main.cpp ^
                 /LIBPATH:C:\vclib\SDL2-2.30.7\lib\x64 ^
                 /LIBPATH:C:\vclib\SDL2_image-2.8.2\lib\x64 ^
                 /LIBPATH:C:\vclib\SDL2_ttf-2.22.0\lib\x64\ ^
-                    shell32.lib ^
                     SDL2.lib ^
                     SDL2main.lib ^
                     SDL2_image.lib ^
                     SDL2_ttf.lib ^
+                    shell32.lib ^
+                    kernel32.lib ^
+                    user32.lib ^
+                    gdi32.lib ^
+                    winspool.lib ^
+                    comdlg32.lib ^
+                    advapi32.lib ^
+                    ole32.lib ^
+                    oleaut32.lib ^
+                    uuid.lib ^
+                    odbc32.lib ^
+                    odbccp32.lib ^
+            /DYNAMICBASE ^
             /SUBSYSTEM:CONSOLE ^
             /out:..\CPP_Engine\main.exe
 popd
